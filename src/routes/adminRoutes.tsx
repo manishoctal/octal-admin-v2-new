1e20528caf20c3df3c11b07bc08bfc5d8e1ef1da
abhishek <abhishek.maurya@octalsoftware.com>
2025-11-19 17:05:38 +0530

    fix

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,30 +1,14 @@
 import { DashboardLayout } from '../components/DashboardLayout';
 import { DashboardOverview } from '../components/DashboardOverview';
 import { UsersManager } from '../components/UsersManager';
-import { UserForm } from '../components/UserForm';
 import { UserDetails } from '../components/UserDetails';
-import { ProductsManager } from '../components/ProductsManager';
-import { ProductForm } from '../components/ProductForm';
-import { ProductDetails } from '../components/ProductDetails';
 import { Settings } from '../components/Settings';
-import { CirclesManager } from '../components/CirclesManager';
-import { CircleForm } from '../components/CircleForm';
-import { CircleDetails } from '../components/CircleDetails';
-import { EventsManager } from '../components/EventsManager';
-import { EventForm } from '../components/EventForm';
-import { EventDetails } from '../components/EventDetails';
-import { IntentionsManager } from '../components/IntentionsManager';
-import { IntentionForm } from '../components/IntentionForm';
-import { IntentionDetails } from '../components/IntentionDetails';
-import { PaymentsManager } from '../components/PaymentsManager';
 import { SubadminManager } from '../components/SubadminManager';
 import { SubadminForm } from '../components/SubadminForm';
 import { SubadminDetails } from '../components/SubadminDetails';
 import { ProtectedRoute } from '../components/ProtectedRoute';
 import SharedLayout from '../components/sharedLayout'
-import { VirtualGiftsManager } from '../components/VirtualGiftsManager';
-import { VirtualGiftForm } from '../components/VirtualGiftForm';
-import { VirtualGiftDetails } from '../components/VirtualGiftDetails';
+
 import ProtectedModuleRoute from './protectedRoutes';
 import { ChangePassword } from '@/components/ChangePassword';
 import { Profile } from '@/components/Profile';
@@ -35,7 +19,7 @@ import { EmailTemplateEditor } from '@/components/EmailTemplateEditor';
 import { StaticContentEditor } from '@/components/StaticContentEditor';
 import { NotificationManager } from '@/components/NotificationManager';
 import { ReportManager } from '@/components/ReportManager';
-import { SecretCrushManager } from '@/components/SecretCrush';
+
 import { ErrorLogsManager } from '@/components/ErrorLogsManager';
  import { UiComponentPreview } from '@/components/CommonUiOverview';
 
